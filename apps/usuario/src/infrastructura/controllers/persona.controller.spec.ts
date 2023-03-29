9c4e71e058616eb1023c10b6fa9a6bf503450d50
Cristian <89285428+Cristiangonzz@users.noreply.github.com>
2023-03-29 20:19:22 -0300

    spec de controlador de persona

--- apps/usuario/src/infrastructura/controllers/persona.controller.spec.ts
+++ apps/usuario/src/infrastructura/controllers/persona.controller.spec.ts
@@ -1,59 +1,101 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { PersonaController } from './persona.controller';
-import { PersonaService } from '../service/persona.service';
-import { RegistrarPersonaDto } from '../dto/registrar-persona.dto';
-import { PersonaSchema } from '../dataBase/schema/persona.shema';
-import { of } from 'rxjs';
-import { PersonaRegistradaPublisher } from '../messanging/publisher/persona/persona-registrado.publisher';
-import { RegistrarPersonaoUseCase } from '../../aplicacion/useCase/persona/registrar-persona.use-case';
-import { PersonaBuscadaPublisher } from '../messanging/publisher/persona/persona-buscada.oublisher';
-
+import { Test, TestingModule } from "@nestjs/testing";
+import { PersonaBuscadaPublisher } from "../messanging/publisher/persona/persona-buscada.oublisher";
+import { PersonaRegistradaPublisher } from "../messanging/publisher/persona/persona-registrado.publisher";
+import { PersonaService } from "../service/persona.service";
+import { PersonaController } from "./persona.controller";
+import { RegistrarPersonaDto } from "../dto/registrar-persona.dto";
+import { PersonaDomainEntity } from "../../dominio/model/persona";
+import { RegistrarPersonaoUseCase } from "../../aplicacion/useCase/persona/registrar-persona.use-case";
+import { of,lastValueFrom } from "rxjs";
 
 describe('PersonaController', () => {
-  let personaController: PersonaController;
-  let personaService: PersonaService;
+
+  let api: PersonaController;
+  let servicio: PersonaService;
+  let eventoCreada: PersonaRegistradaPublisher;
+  let eventoBuscar: PersonaBuscadaPublisher;
+
+  const _id = '641c65deff0153dd0f36bf5';
 
   beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
-      controllers: [PersonaController],
+    const module: TestingModule = await Test.createTestingModule({
       providers: [
         {
           provide: PersonaService,
-          useValue: {
-            registar: jest.fn(),
-            findOneBy: jest.fn(),
-          },
+          useValue: {},
         },
         {
             provide: PersonaRegistradaPublisher,
             useValue: {
-                publish: jest.fn(),
+              publish : jest.fn()
             },
         },
         {
-            provide: PersonaBuscadaPublisher,
-            useValue: {
-                publish: jest.fn(),
-            },
-        },
+          provide: PersonaBuscadaPublisher,
+          useValue: {
+            publish : jest.fn()
+          },
+      },
       ],
+      controllers: [PersonaController],
     }).compile();
 
-
-    personaService = app.get<PersonaService>(PersonaService);
-    personaController = app.get<PersonaController>(PersonaController);
+    api = module.get<PersonaController>(PersonaController);
+    servicio = module.get<PersonaService>(PersonaService);
+    eventoCreada = module.get<PersonaRegistradaPublisher>(PersonaRegistradaPublisher);
+    eventoBuscar = module.get<PersonaBuscadaPublisher>(PersonaBuscadaPublisher);
   });
 
-//   it('should return "Hello World!"', () => {
-//     // Arrange
-//     const mockData = 'Hello World!';
-//     const expected = 'Hello World!';
-//     jest.spyOn(PersonaService, 'registar').mockReturnValue(mockData);
-//     // Act
-//     const result = appController.getHello();
-//     // Assert
-//     expect(result).toEqual(expected);
-//     expect(PersonaService.getHello).toHaveBeenCalled();
-//   });
 
-});
+  it('should be defined', () => {
+    expect(api).toBeDefined();
+    expect(servicio).toBeDefined();
+    expect(eventoCreada).toBeDefined();
+    expect(eventoBuscar).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('debe crear una nueva VideoConferencia', async () => {
+      // Arrange
+      const persona:RegistrarPersonaDto = {
+          nombre: "Cristian",              
+          mail:  "cris@gmail.com",
+          clave: "123456",
+          setPassword: expect.any(Function),
+        }
+
+      const mockaPersona : PersonaDomainEntity= 
+        {
+          nombre: "Cristian",              
+          mail:  "cris@gmail.com",
+          clave: "123456",
+          setPassword: expect.any(Function),
+        };
+
+      const expectedPersona:PersonaDomainEntity = 
+        {
+          nombre: "Cristian",              
+          mail:  "cris@gmail.com",
+          clave: "123456",
+          setPassword: expect.any(Function),
+        };
+
+      //Mockear el caso de uso y el publisher
+     jest
+      .spyOn(RegistrarPersonaoUseCase.prototype, 'execute')
+      .mockReturnValue(of(mockaPersona));
+
+    jest.spyOn(eventoCreada, 'publish').mockReturnValue(of(persona));
+
+    // Act
+
+    const result = api.crearPersona(persona);
+
+    // Assert
+    expect(await lastValueFrom(result) ).toEqual((expectedPersona));
+     })
+      });
+  });
+
+
+ 
