93bd72fb096afd046025a2dc3fabaad5d362a8e9
Cristian <89285428+Cristiangonzz@users.noreply.github.com>
2023-03-30 19:50:27 -0300

    spec de repo completo

--- apps/usuario/src/dominio/services/persona.domain.service.spec.ts
+++ apps/usuario/src/dominio/services/persona.domain.service.spec.ts
@@ -2,6 +2,8 @@
 import { of ,Observable} from 'rxjs';
 import { IPersonaDomainService } from './persona.domain.service';
 import { PersonaDomainEntity } from '../model/persona';
+import { Type } from 'class-transformer';
+import { Types } from 'mongoose';
 
 
 describe('IPersonaDomainService', () => {
@@ -11,6 +13,8 @@ describe('IPersonaDomainService', () => {
     personaDomainService = {
       registar: jest.fn(),
       findOneBy: jest.fn(),
+      actualizar: jest.fn(),
+      eliminar: jest.fn(),
     };
   });
 
@@ -76,4 +80,72 @@ describe('IPersonaDomainService', () => {
        expect(result).toBeInstanceOf(Observable);
        result.subscribe((value) => expect(value).toEqual(mockReturnValue));
      });
+
+      
+     describe('Editar', () => {
+      it('should call Editar method', () => {
+        const _id = new Types.ObjectId("641f1e79398d97022720784b");
+        const persona : PersonaDomainEntity = 
+      { 
+    
+          nombre: "uruguay",
+          mail: "uru@gmail.com",
+          clave: "123",
+          setPassword: expect.any(Function),
+      
+      
+      };
+        personaDomainService.actualizar(_id.toString(),persona);
+        expect(personaDomainService.actualizar).toHaveBeenCalledWith(_id.toString(),persona);
+      
+      });
+  
+      it('should return an Observable of type T', () => {
+        const _id = new Types.ObjectId("641f1e79398d97022720784b");
+        const persona: PersonaDomainEntity =
+          { 
+              nombre: "uruguay",
+              mail: "uru@gmail.com",
+              clave: "123",
+              setPassword: expect.any(Function),
+              
+          };
+        const mockReturnValue: PersonaDomainEntity = 
+          { 
+              nombre: "uruguay",
+              mail: "uru@gmail.com",
+              clave: "123",
+              setPassword: expect.any(Function),
+              
+          };
+        (personaDomainService.actualizar as jest.Mock).mockReturnValue(of(mockReturnValue));
+        const result = personaDomainService.actualizar(_id.toString(),persona);
+        expect(result).toBeInstanceOf(Observable);
+        result.subscribe((value) => expect(value).toEqual(mockReturnValue));
+      });
+    });
+  
+
+
+
+    describe('Eliminar', () => {
+      it('should call Eliminar method', () => {
+        const _id = new Types.ObjectId("641f1e79398d97022720784b");
+      
+        personaDomainService.eliminar(_id.toString());
+        expect(personaDomainService.eliminar).toHaveBeenCalledWith(_id.toString());
+      
+      });
+  
+      it('should return an Observable of type T', () => {
+        const _id = new Types.ObjectId("641f1e79398d97022720784b");
+        const mock: boolean = true;
+       
+        (personaDomainService.eliminar as jest.Mock).mockReturnValue(of(mock));
+        const result = personaDomainService.eliminar(_id.toString());
+
+        expect(result).toBeInstanceOf(Observable);
+        result.subscribe((value) => expect(value).toEqual(mock));
+      });
+    });
    });
--- apps/usuario/src/infrastructura/dataBase/services/persona.service.mongo.spec.ts
+++ apps/usuario/src/infrastructura/dataBase/services/persona.service.mongo.spec.ts
@@ -3,6 +3,7 @@ import { PersonaMongoService } from './persona.service.mongo';
 import { PersonaRepository } from '../repository/persona.repositoy';
 import { PersonaSchema } from '../schema/persona.shema';
 import { lastValueFrom, of } from 'rxjs';
+import { Types } from 'mongoose';
 
 
 describe('PersonaMongoService', () => {
@@ -18,6 +19,9 @@ describe('PersonaMongoService', () => {
           useValue: {
             registar: jest.fn(),
             findOneBy: jest.fn(),
+            actualizar: jest.fn(),
+            eliminar: jest.fn(),
+            
           },
         },
       ],
@@ -114,4 +118,81 @@ describe('PersonaMongoService', () => {
     expect(await lastValueFrom(result)).toEqual(expected);
     expect(personaRepository.registar).toHaveBeenCalled();
   });
+
+
+  describe('actualizar', () => {
+
+    it('Actualizar tiene que retornar "Observable<PersonaChema>"', async () =>  {
+      // Arrange
+  
+      const _id = new Types.ObjectId("6425e193cc50b70709e09bfe");
+      const persona : PersonaSchema=(
+        {
+          nombre : "cristian",
+          mail : "cris@gmail.com",
+          clave : "123456",
+          setPassword: expect.any(Function)
+        }
+      );
+  
+      const mockData =(
+        {
+          _id : "6425e193cc50b70709e09bfe",
+          nombre : "cristian",
+          mail : "cris@gmail.com",
+          clave : "123456"
+        }
+      );
+    
+  
+      const expected =(
+        {
+          _id : "6425e193cc50b70709e09bfe",
+          nombre : "cristian",
+          mail : "cris@gmail.com",
+          clave : "123456"
+        }
+      );
+     
+  
+  
+      jest.spyOn(personaRepository, 'actualizar').mockReturnValue(of(mockData) as any);
+  
+      // Act
+      const result = personaMongoService.actualizar(_id.toString(),persona);
+  
+      // Assert
+      expect(await lastValueFrom(result)).toEqual(expected);
+      expect(personaRepository.actualizar).toHaveBeenCalled();
+    });
+  });
+
+
+  
+  describe('Eliminar', () => {
+
+    it('Eliminar tiene que retornar "Observable<boolean>"', async () =>  {
+      // Arrange
+  
+      const _id = new Types.ObjectId("6425e193cc50b70709e09bfe");
+      const mock  : boolean = true;
+  
+      
+  
+      const expected : boolean = true;
+     
+  
+  
+      jest.spyOn(personaRepository, 'eliminar').mockReturnValue(of(mock) as any);
+  
+      // Act
+      const result = personaMongoService.eliminar(_id.toString());
+  
+      // Assert
+      expect(await lastValueFrom(result)).toEqual(expected);
+      expect(personaRepository.eliminar).toHaveBeenCalled();
+    });
+  });
+
+  
 });
